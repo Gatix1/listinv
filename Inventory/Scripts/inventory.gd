4a255f35b2aef992ab51cc0ea0f4cf00a918ad94
Maxim Pintea <maxim.pintea7@gmail.com>
2024-07-23 16:29:17 +0300

    Experimental saving support added

--- Inventory/Scripts/inventory.gd
+++ Inventory/Scripts/inventory.gd
@@ -1,7 +1,10 @@
 class_name Inventory
 extends Node2D
 
-var inventory := [] # List to store inv items
+var save_file_path = ""
+var save_file_name = "PlayerSave.tres"
+var playerData = PlayerData.new()
+
 @export var inventory_ui : InventoryUI
 @onready var sound_player : SoundPlayer = $CanvasLayer/SoundPlayer 
 @onready var music_player : MusicPlayer = $CanvasLayer/MusicPlayer 
@@ -14,22 +17,37 @@ func _process(delta):
 		add_item(load('res://Inventory/Items/test_item.tres'))
 		add_item(load('res://Inventory/Items/test_item2.tres'))
 		inventory_ui.render_items()
+	if Input.is_action_just_pressed("save"):
+		save()
+	if Input.is_action_just_pressed("load"):
+		load_data()
+
+func resource_to_dict(item : ItemResource) -> Dictionary:
+	return {"name": item.name, "description": item.description, "action": item.action, "quantity": item.quantity};
+
+func load_data():
+	playerData = ResourceLoader.load(save_file_path + save_file_name)
+	print(playerData.inventory[0].quantity)
+
+func save():
+	print(playerData.inventory[0].quantity)
+	ResourceSaver.save(playerData, save_file_path + save_file_name)
 
 #---Function adds a new item to the inventory---#
 func add_item(item : ItemResource) -> void:
-	for index in range(inventory.size()):
-		if item.name == inventory[index].name:
-			inventory[index].quantity += 1
+	for index in range(playerData.inventory.size()):
+		if item.name == playerData.inventory[index].name:
+			playerData.inventory[index].quantity += 1
 			return
-	inventory.append(item)
+	playerData.inventory.append(resource_to_dict(item))
 
 #---Function that removes an item from the inventory---#
-func remove_item(item : ItemResource) -> void:
-	for index in range(inventory.size()):
-		if item.name == inventory[index].name:
+func remove_item(item : Dictionary) -> void:
+	for index in range(playerData.inventory.size()):
+		if item.name == playerData.inventory[index].name:
 			if item.quantity > 1:
-				inventory[index].quantity -= 1
+				playerData.inventory[index].quantity -= 1
 			else:
-				inventory.erase(item)
+				playerData.inventory.erase(item)
 			return
 
--- Inventory/Scripts/inventory_ui.gd
+++ Inventory/Scripts/inventory_ui.gd
@@ -29,20 +29,20 @@ func _process(delta):
 		inventory.sound_player.play_sound(inventory.accept_select_sound)
 		if more_info_panel.visible:
 			if more_info_panel.selected_option == more_info_panel.options.DROP:
-				inventory.remove_item(inventory.inventory[selected_item])
+				inventory.remove_item(inventory.playerData.inventory[selected_item])
 				more_info_panel.visible = false
 				render_items()
 				render_selected()  
 			elif more_info_panel.selected_option == more_info_panel.options.USE:
-				if inventory.inventory[selected_item].action != "":
-					items_actions.call_deferred(inventory.inventory[selected_item].action)
+				if inventory.playerData.inventory[selected_item].action != "":
+					items_actions.call_deferred(inventory.playerData.inventory[selected_item].action)
 		else:
 			more_info_panel.visible = true
 		
 	if not canvas.visible: return
 	if more_info_panel.visible: return
 	
-	if Input.is_action_just_pressed("ui_down") and selected_item < inventory.inventory.size()-1:
+	if Input.is_action_just_pressed("ui_down") and selected_item < inventory.playerData.inventory.size()-1:
 		selected_item += 1
 		inventory.sound_player.play_sound(inventory.change_select_sound)
 		render_selected()
@@ -53,27 +53,27 @@ func _process(delta):
 			
 
 func render_selected():
-	if selected_item > inventory.inventory.size()-1 or selected_item < 0:
+	if selected_item > inventory.playerData.inventory.size()-1 or selected_item < 0:
 		selected_item = 0
 		
 	for i in range(container.get_children().size()):
 		if selected_item == i:
-			more_info_panel.item_name = inventory.inventory[i].name
-			more_info_panel.item_description = inventory.inventory[i].description
+			more_info_panel.item_name = inventory.playerData.inventory[i].name
+			more_info_panel.item_description = inventory.playerData.inventory[i].description
 			container.get_children()[i].selected = true
 			for j in range(container.get_children().size()):
 				if selected_item != j:
 					container.get_children()[j].selected = false
 
 func render_items():
-	if inventory.inventory.size() == 0:
+	if inventory.playerData.inventory.size() == 0:
 		more_info_panel.visible = false
 		
 	for child in container.get_children():
 		container.remove_child(child)
-	for i in range(inventory.inventory.size()):
+	for i in range(inventory.playerData.inventory.size()):
 		var ui_item = preload("res://Inventory/inventory_ui_item.tscn")
 		var ui_item_instance = ui_item.instantiate()
-		ui_item_instance.item_name = inventory.inventory[i].name
-		ui_item_instance.item_count = inventory.inventory[i].quantity
+		ui_item_instance.item_name = inventory.playerData.inventory[i].name
+		ui_item_instance.item_count = inventory.playerData.inventory[i].quantity
 		container.add_child(ui_item_instance)
